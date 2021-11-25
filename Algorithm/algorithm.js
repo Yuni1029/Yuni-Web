865eea9ecf4538700800753ee853a9cc80b500b7
Yuni1029 <2024yunishin@usbccollegiate.org>
2021-11-25 11:35:35 +0900

    first commit

---
+++ Algorithm/algorithm.js
@@ -0,0 +1,49 @@
+//question 1
+function y (n, x) {
+    return(n + x)
+}
+let addition = y (2, 3) 
+console.log(addition)
+
+//question 2
+function Area(π, r) {
+    return(π*r*r)
+}
+let Circle = Area(3.14,3)
+console.log(Circle)
+
+//question 3
+function fx (h,k){
+    if (h+k < 100) {
+        return true;
+    } else  {
+        return false;
+    }
+}
+
+let trueFalse = fx(33,45)
+console.log(trueFalse)
+
+//question 4
+function year(a) {
+    if (a % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let leapYear = year(2020)
+console.log(leapYear)
+
+//question 5
+function integer(c,d) {
+    if (c+d===10 || c===10 || d===10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let sum = integer(9, 10)
+console.log(sum)
